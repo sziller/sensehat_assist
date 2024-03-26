981c7dd760557fe5500074454eb2a0f5e2d2556f
sziller <szillerke@gmail.com>
2024-03-26 20:43:06 +0100

    new installer, corrected error msg

--- SenseHatCustomExceptions/DisplayErrors.py
+++ SenseHatCustomExceptions/DisplayErrors.py
@@ -5,6 +5,7 @@ class MissingDisplay(Exception):
     """=== Exception name: MissingDisplay ==============================================================================
     Exception should be used when Sense hat stle display or Emulator not found to import.
     ============================================================================================== by Sziller ==="""
-    def __init__(self, message="Display not found: you either need sense_hat or sense_emu."):
+    def __init__(self, message="Display not found: you either need sense_hat or sense_emu.\n"
+                               "for further help see package README.adoc"):
         self.message = message
         super().__init__(self.message)
